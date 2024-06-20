c81e0fd55d6a4c4cfd6e0a7ac188948ba05c2ffb
brunomachadors <brunomachadors@gmail.com>
2024-06-20 11:35:37 +0100

    setting new viewportsize to width 1600 and height 900

--- tests/searchPokemon.spec.js
+++ tests/searchPokemon.spec.js
@@ -1,7 +1,8 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Search Gengar', async({page}) => {
+test('Search Gengar', async ({ page }) => {
+  await page.setViewportSize({ width: 1600, height: 900 });
   await page.goto('');
   await expect(page).toHaveTitle(/Pokedex/);
 });
