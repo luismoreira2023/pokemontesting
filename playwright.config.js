bb90f03e2ac3d511c0f9b191be54305f27149c8f
luismoreira2023 <luis.moreira@school.mindera.com>
2024-06-20 16:05:15 +0100

    searchPokemon.spec.js

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  timeout: 90000,
+  timeout: 900000,
   expect: {
     timeout: 30000,
   },
