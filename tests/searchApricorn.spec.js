4ee448c3ff316f76a3e4cc23cf3ae0b1df1398b9
luismoreira2023 <luis.moreira@school.mindera.com>
2024-07-11 10:16:21 +0100

    updated apricorns

--- tests/searchApricorn.spec.js
+++ tests/searchApricorn.spec.js
@@ -26,75 +26,110 @@ const apricornBall = async (page, ball) => {
 };
 
 test.describe.skip('Apricorn tests', ()=>{
-
-
   test('Search Red Apricorn', async ({ page }) => {
     await selectApricorn(page, 'red-apricorn');
     await expect(page.getByText('Used to make a Level Ball.')).toHaveText('Used to make a Level Ball.');
 });
-
-test('Search Level Ball', async ({ page }) => {
-  await apricornBall(page, 'level-ball');
-  await expect(page.getByText('Success rate based off of')).toHaveText("Success rate based off of fraction target Pokémon is of user's Pokémon. Made from Red Apricorn.");
+  test('Search Level Ball', async ({ page }) => {
+    await apricornBall(page, 'level-ball');
+    await expect(page.getByText('Success rate based off of')).toHaveText("Success rate based off of fraction target Pokémon is of user's Pokémon. Made from Red Apricorn.");
+});
 });
 
-test('Search Blue Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'blue-apricorn');
-  await expect(page.getByText('Used to make a Lure Ball.')).toHaveText("Used to make a Lure Ball.");
+test('Get Red Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/red-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Level Ball.");
 });
 
-test('Search Lure Ball', async ({ page }) => {
-  await apricornBall(page, 'lure-ball');
-  await expect(page.getByText('3× effectiveness while')).toHaveText("3× effectiveness while fishing. Made from Blu Apricorn.");
+test('Get Level Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/level-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Success rate based off of fraction target Pokémon is of user's Pokémon. Made from Red Apricorn.");
 });
 
-test('Search Yellow Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'yellow-apricorn');
-  await expect(page.getByText('Used to make a Moon Ball.')).toHaveText("Used to make a Moon Ball.");
+test('Get Blue Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/blue-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Lure Ball.");
 });
 
-test('Search Moon Ball', async ({ page }) => {
-  await apricornBall(page, 'moon-ball');
-  await expect(page.getByText('4× effectiveness on familes')).toHaveText("4× effectiveness on familes of Pokémon with a Moon Stone evolution. Made from Ylw Apricorn.");
+test('Get Lure Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/lure-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("3× effectiveness while fishing. Made from Blu Apricorn.");
 });
 
-test('Search Green Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'green-apricorn');
-  await expect(page.getByText('Used to make a Friend Ball.')).toHaveText("Used to make a Friend Ball.");
+test('Get Yellow Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/yellow-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Moon Ball.");
 });
 
-test('Search Friend Ball', async ({ page }) => {
-  await apricornBall(page, 'friend-ball');
-  await expect(page.getByText('Caught Pokémon start with 200')).toHaveText("Caught Pokémon start with 200 happiness. Made from Grn Apricorn.");
+test('Get Moon Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/moon-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("4× effectiveness on familes of Pokémon with a Moon Stone evolution. Made from Ylw Apricorn.");
 });
 
-test('Search Pink Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'pink-apricorn');
-  await expect(page.getByText('Used to make a Love Ball.')).toHaveText("Used to make a Love Ball.");
+test('Get Green Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/green-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Friend Ball.");
 });
 
-test('Search Love Ball', async ({ page }) => {
-  await apricornBall(page, 'love-ball');
-  await expect(page.getByText('8× effectiveness on opposite')).toHaveText("8× effectiveness on opposite sex, same species targets of the Active Pokémon. Made from Pnk Apricorn.");
+test('Get Friend Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/friend-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Caught Pokémon start with 200 happiness. Made from Grn Apricorn.");
 });
 
-test('Search White Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'white-apricorn');
-  await expect(page.getByText('Used to make a Fast Ball.')).toHaveText("Used to make a Fast Ball.");
+test('Get Pink Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/pink-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Love Ball.");
 });
 
-test('Search Fast Ball', async ({ page }) => {
-  await apricornBall(page, 'fast-ball');
-  await expect(page.getByText('4× effectiveness on Pokémon')).toHaveText("4× effectiveness on Pokémon with 100 or greater base speed. (Gen II: Roaming or Fleeing Pokémon). Made from Wht Apricorn.");
+test('Get Love Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/love-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("8× effectiveness on opposite sex, same species targets of the Active Pokémon. Made from Pnk Apricorn.");
 });
 
-test('Search Black Apricorn', async ({ page }) => {
-  await selectApricorn(page, 'black-apricorn');
-  await expect(page.getByText('Used to make a Heavy Ball.')).toHaveText("Used to make a Heavy Ball.");
+test('Get White Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/white-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Fast Ball.");
 });
 
-test('Search Heavy Ball', async ({ page }) => {
-  await apricornBall(page, 'heavy-ball');
-  await expect(page.getByText('Has flat bonus or penalty to')).toHaveText("Has flat bonus or penalty to catch rate depending on weight class of target. Made from Blk Apricorn.");
+test('Get Fast Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/fast-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("4× effectiveness on Pokémon with 100 or greater base speed. (Gen II: Roaming or Fleeing Pokémon). Made from Wht Apricorn.");
 });
-})
+
+test('Get Black Apricorn', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/black-apricorn');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Used to make a Heavy Ball.");
+});
+
+test('Get Heavy Ball', async ({ page }) => {
+  const response = await page.request.get('https://pokeapi.co/api/v2/item/heavy-ball');
+  const item = await response.json();
+  console.log(item.effect_entries[0].short_effect);
+  await expect(item.effect_entries[0].short_effect).toBe("Has flat bonus or penalty to catch rate depending on weight class of target. Made from Blk Apricorn.");
+});
