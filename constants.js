497641e805a41e93923bbcbd8432736508963a6b
luismoreira2023 <luis.moreira@school.mindera.com>
2024-07-18 16:28:30 +0100

    new evolutions

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const EEVEE_EVOLUTIONS = [
+  'vaporeon', 'jolteon', 'flareon', 'espeon', 'umbreon', 'leafeon', 'glaceon', 'sylveon'
+];
+export const TYROGUE_EVOLUTIONS = ['hitmonlee', 'hitmonchan', 'hitmontop'];
